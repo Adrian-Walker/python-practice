e0f81ed49d23615afed91f2e883f0891b133fbba
Adrian Walker <awalker3365@gmail.com>
2021-09-09 18:27:04 -0400

    9-9-21 627p

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-age = input("How old are you? ")
-print("I am " + age + " years old")
+# age = input("How old are you? ")
+# print("I am " + age + " years old")
+
+birthday = input("enter ur birthday: ")
+# int(birthday)
+age = 2021 - int(birthday)
+print(age)
