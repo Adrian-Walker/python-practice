80b24d481ccb7e99b63b2d07e1cd1670ce004840
Adrian Walker <awalker3365@gmail.com>
2021-09-09 19:00:40 -0400

    9-9-21 7p

--- app.py
+++ app.py
@@ -1,13 +1,5 @@
-# age = input("How old are you? ")
-# print("I am " + age + " years old")
-
-# birthday = input("enter ur birthday: ")
-# # int(birthday)
-# age = 2021 - int(birthday)
-# print(age)
-
 firstNum = input("First: ")
 secondNum = input("Second: ")
 
 finalNum = int(firstNum) + int(secondNum)
-print("Sum: " + str(finalNum))
+print("Equals: " + str(finalNum))
